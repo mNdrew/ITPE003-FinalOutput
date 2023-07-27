50bb6e9dc95beadf7708fa00337782027c81c728
mNdrew <mandrew1425@gmail.com>
2023-07-28 00:35:08 +0800

    12:30 Backup

--- shopRunner.js
+++ shopRunner.js
@@ -8,6 +8,8 @@ const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/img', express.static(path.join(__dirname, 'public/img')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+
 
 const uri = 'mongodb://127.0.0.1:27017/ITPE003-FinalOutput';
 const options = {
@@ -16,9 +18,30 @@ const options = {
 };
 
 app.set('view engine', 'ejs');
-app.set('view', __dirname + '/views');
+app.set('views', __dirname + '/views');
+
 
 app.get('/', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('shopItems');
+    
+    // Fetch data from MongoDB
+    const data = await collection.find({}).toArray();
+
+    // Render the EJS template and pass the data
+    res.render('homepage', { data });
+
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/shop', async (req, res) => {
   try {
     const client = await MongoClient.connect(uri, options);
     const db = client.db();
@@ -29,6 +52,105 @@ app.get('/', async (req, res) => {
 
     // Render the EJS template and pass the data
     res.render('shop', { data });
+
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/cart', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('shopItems');
+    
+    // Fetch data from MongoDB
+    const data = await collection.find({}).toArray();
+
+    // Render the EJS template and pass the data
+    res.render('myCart', { data });
+
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/mycart', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('shopItems');
+    
+    // Fetch data from MongoDB
+    const data = await collection.find({}).toArray();
+
+    // Render the EJS template and pass the data
+    res.render('myCart', { data });
+
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/myaccount', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('shopItems');
+    
+    // Fetch data from MongoDB
+    const data = await collection.find({}).toArray();
+
+    // Render the EJS template and pass the data
+    res.render('user-account', { data });
+
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/about', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('shopItems');
+    
+    // Fetch data from MongoDB
+    const data = await collection.find({}).toArray();
+
+    // Render the EJS template and pass the data
+    res.render('about', { data });
+
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+app.get('/dashboard-admin', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('shopItems');
+    
+    // Fetch data from MongoDB
+    const data = await collection.find({}).toArray();
+
+    // Render the EJS template and pass the data
+    res.render('dashboard-admin', { data });
 
     // Remember to close the MongoDB connection
     client.close();
