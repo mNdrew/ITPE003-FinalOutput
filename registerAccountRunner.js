b571f5bc744e05432146a393446751c60208d25e
mNdrew <mandrew1425@gmail.com>
2023-07-29 17:38:41 +0800

    Saturday Closing

--- registerAccountRunner.js
+++ registerAccountRunner.js
@@ -10,6 +10,7 @@ const port = 3030;
 const url = 'mongodb://127.0.0.1:27017';
 const dbName = 'ITPE003-FinalOutput';
 const collectionName = 'users';
+const messagesCollectionName = 'messages';
 let db;
 
 const uri = 'mongodb://127.0.0.1:27017/ITPE003-FinalOutput';
@@ -40,14 +41,46 @@ app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.get('/', (req, res) => {
-  // render homepage.ejs
-  res.render(path.join(__dirname, '/views/homepage.ejs'));
+app.get('/', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('users');
+
+
+    // Fetch data from MongoDB
+    const itemData = await collection.find({objectType: 'shopItem' }).limit(6).toArray();
+
+    // Render the EJS template and pass the data
+    res.render('homepage', { itemData});
+
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-app.get('/home', (req, res) => {
-  // render homepage.ejs
-  res.render(path.join(__dirname, '/views/homepage.ejs'));
+app.get('/home', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('users');
+
+
+    // Fetch data from MongoDB
+    const itemData = await collection.find({objectType: 'shopItem' }).limit(3).toArray();
+
+    // Render the EJS template and pass the data
+    res.render('homepage', { itemData});
+
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.get('/about', async (req, res) => {
@@ -59,7 +92,7 @@ app.get('/about', async (req, res) => {
 
 
       // Fetch data from MongoDB
-      const itemData = await collection.find({objectType: 'shopItem' }).limit(6).toArray();
+      const itemData = await collection.find({objectType: 'shopItem' }).limit(3).toArray();
 
       // Render the EJS template and pass the data
       res.render('about', { itemData});
@@ -228,29 +261,27 @@ app.get('/shop-dashboard', async (req, res) => {
   }
 });
 
+
 app.get('/contact', async (req, res) => {
-  if(myUserObjectType != 'admin') {
-    try {
-      const client = await MongoClient.connect(uri, options);
-      const db = client.db();
-      const collection = db.collection('users');
-      
-      // Fetch data from MongoDB
-      const data = await collection.find({objectType: 'shopItem'}).toArray();
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('shopItems');
+    
+    // Fetch data from MongoDB
+    const data = await collection.find({}).toArray();
 
-      // Render the EJS template and pass the data
-      res.render('contactus', { data });
+    // Render the EJS template and pass the data
+    res.render('contactus', { data });
 
-      // Remember to close the MongoDB connection
-      client.close();
-    } catch (err) {
-      console.error('Error fetching data from MongoDB:', err);
-      res.status(500).send('Internal Server Error');
-    } 
-  } else {
-    res.redirect('/myAccount');
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
   }
 });
+
 
 app.get('/myAccount', async (req, res) => {
   //verify if myObject type is user
@@ -263,13 +294,12 @@ app.get('/myAccount', async (req, res) => {
       // Fetch data from MongoDB
       const userInfo = await collection.find({_id: myUserID}).toArray();
       console.log(userInfo);
-      const formattedOutput = JSON.stringify(userInfo, null, 2);
-      convert formatttedOutput to array
+      // const formattedOutput = JSON.stringify(userInfo, null, 2);
 
-      console.log(formattedOutput)
+      // console.log(formattedOutput)
 
       // Render the EJS template and pass the data
-      res.render('myAccount', { formattedOutput });
+      res.render('myAccount'/*, { formattedOutput }*/);
   
       // Remember to close the MongoDB connection
       client.close();
@@ -323,19 +353,38 @@ app.get('/shop', async (req, res) => {
 });
 
 app.get('/mycart', async (req, res) => {
+  if(myUserObjectType === 'user'){
+    try {
+      const client = await MongoClient.connect(uri, options);
+      const db = client.db();
+      const collection = db.collection('userCarts');
 
+      // Fetch data from MongoDB
+      const dataCart = await collection.find({owner: myUserID}).toArray();
+      console.log(dataCart);
+
+      // Render the EJS template and pass the data
+      res.render('mycart', { dataCart });
+
+      // Remember to close the MongoDB connection
+      // client.close();
+    } catch (err) {
+      console.error('Error fetching data from MongoDB:', err);
+      res.status(500).send('Internal Server Error');
+    }
+  } else {
+    // const dataCart = await collection.find({owner: myUserID}).toArray();
+    res.redirect('/login-user');
+  }
 });
 
 app.post('/usr-addToCart', async (req, res) => {
-  if(myUserObjectType == 'user') {
-    const { itmID, itmQty } = req.body;
+  if(myUserObjectType === '' || myUserObjectType == null || myUserObjectType == 'admin') {
+    return;
+  }
+  else {
+    const {itemId, itemImage, itemTitle, itemPrice, itemQuantity, orderPrice} = req.body;
     console.log('Received request body:', req.body);
-    
-    // GET itmID and itmQty from the request body
-    const itmID2 = req.body.itmID;
-    const itmQty2 = req.body.itmQty;
-    console.log(itmID2);
-    console.log(itmQty2);
 
     try {
       if (!db) {
@@ -343,76 +392,64 @@ app.post('/usr-addToCart', async (req, res) => {
         return res.status(500).json({ error: 'Database connection is not ready.' });
       }
 
-      let pushquery ="" 
-      // combine the query that will be pushed to the array
-      pushquery = pushquery.concat("id: '", itmID, "', quantity: ", itmQty);
-      console.log(pushquery);
+      console.log('Inserting record:', req.body);
+      // let itemId = itemId.valueOf();
 
-      //update the carItems array variable for the user
-      const result = await db.collection(collectionName).updateOne(
-        { _id: myUserID },
-        // insert itmID and itmQty into the cartItems array variable
-        { $push: { cartItems: req.body }}
-      );
+      await db.collection('userCarts').insertOne({
+        // INSERT USER OBJECT TYPE
+        onwer: myUserID,
+        cartItemID: itemId,
+        cartItemImage: itemImage,
+        cartItemTitle: itemTitle,
+        cartItemPrice: itemPrice,
+        cartItemQuantity: itemQuantity,
+        cartItemOrderPrice: orderPrice,
+        objectType: 'cartItem'
+      });
 
-      
+      console.log('Record Inserted Successfully!' + req.body);
+      res.status(201).json({ message: 'Record inserted successfully!' });
+    } catch (err) 
 
-      // print the cartItems 
-      console.log(myUserID);
-
-      console.log('Added to Successfully!', req.body);
-      res.status(201).json({ message: 'Added to successfully!' });
-      
-      updateStocksLeft(itmID, itmQty);
-
-    } catch (err) {
-      console.error('An error occurred while inserting the record.', err);
-      return res.status(500).json({ error: 'An error occurred while inserting the record.' });
+    {
+      console.error('Error inserting record:', err);
+      res.status(500).json({ error: 'Error inserting record:' + err });
     }
-  } else {
-    res.redirect('/login-user');
-
   }
 });
 
 
-async function updateStocksLeft(id, quantity) {
-  try {
-    if (!db) {
-      console.log('Database connection is not established yet.');
-      return { success: false, message: 'Database connection is not ready.' };
-    }
-
-    // Make sure 'quantity' is a positive integer
-    quantity = parseInt(quantity, 10);
-    if (isNaN(quantity) || quantity <= 0) {
-      return { success: false, message: 'Please provide a valid positive quantity.' };
-    }
-
-    // Find the product by 'id'
-    const product = await db.collection('products').findOne({ id });
-
-    if (!product) {
-      return { success: false, message: 'Product not found.' };
-    }
-
-    // Calculate the new 'stocksLeft' value
-    const updatedStocksLeft = Math.max(product.stocksLeft - quantity, 0);
-
-    // Update the 'stocksLeft' in the database
-    await db.collection('products').updateOne(
-      { id },
-      { $set: { stocksLeft: updatedStocksLeft } }
-    );
-
-    return { success: true, message: 'Stocks updated successfully.' };
-  } catch (err) {
-    console.error('An error occurred while updating stocksLeft:', err);
-    return { success: false, message: 'An error occurred while updating stocksLeft.' };
-  }
-}
 
 
+
+    app.post('/contact-form-submit', async (req, res) => {
+      const { name, email, message } = req.body;
+      console.log('Received contact form submission:', req.body);
+      
+      
+
+      try {
+        if (!db) {
+          console.log('Database connection is not established yet.');
+          return res.status(500).json({ error: 'Database connection is not ready.' });
+        }
+
+        console.log('Inserting message into the "messages" collection:', req.body)
+
+        // Save the contact form data to the database
+        await db.collection('messages').insertOne({
+          name: name,
+          email: email,
+          message: message,
+        });
+
+        console.log('Message inserted into the "messages" collection successfully!');
+        res.status(201).json({ message: 'Message sent successfully!' });
+      } catch (err) {
+        console.error('Error inserting message into the "messages" collection:', err);
+        res.status(500).json({ error: 'An error occurred while sending the message.' });
+      }
+    });
 
 
 
--- shopRunner2.js
+++ shopRunner2.js
@@ -3,13 +3,15 @@ const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 const path = require('path');
 const { get } = require('http');
-// const session = require('express-session');
+const session = require('express-session');
 const app = express();
-const port = 3030;
+const port = 5000;
 
 const url = 'mongodb://127.0.0.1:27017';
 const dbName = 'ITPE003-FinalOutput';
 const collectionName = 'users';
+const messagesCollectionName = 'messages';
+
 let db;
 
 const uri = 'mongodb://127.0.0.1:27017/ITPE003-FinalOutput';
@@ -32,9 +34,12 @@ async function connectToDatabase() {
   const client = await MongoClient.connect(url, { useUnifiedTopology: true });
   db = client.db(dbName);
 
-  // Create a messages collection if it doesn't exist
-  await db.createCollection('messages');
+  
 }
+
+
+
+
 
 app.use(express.json());
 app.use(cors());
@@ -59,50 +64,7 @@ app.get('/home', (req, res) => {
 
 
 
-app.post('/contact-form-submit', async (req, res) => {
-  const { name, email, message } = req.body;
-  console.log('Received request body:', req.body);
 
-  try {
-    if (!db) {
-      console.log('Database connection is not established yet.');
-      return res.status(500).json({ error: 'Database connection is not ready.' });
-    }
-
-    await db.collection('messages').insertOne({
-      name,
-      email,
-      message,
-      timestamp: new Date(), // Optional: You can add a timestamp to record when the message was submitted.
-    });
-
-    console.log('Message Inserted Successfully!', req.body);
-    res.status(201).json({ message: 'Message inserted successfully!' });
-  } catch (err) {
-    console.error('Error inserting message:', err);
-    return res.status(500).json({ error: 'An error occurred while inserting the message.' });
-  }
-});
-
-
-app.get('/contact', async (req, res) => {
-  try {
-    // Ensure the database connection is established before fetching messages
-    if (!db) {
-      console.log('Database connection is not established yet.');
-      return res.status(500).json({ error: 'Database connection is not ready.' });
-    }
-
-    // Fetch all messages from the messages collection
-    const messages = await db.collection('messages').find({}).toArray();
-
-    // Render the EJS template and pass the messages data
-    res.render('contactus', { messages });
-  } catch (err) {
-    console.error('Error fetching messages:', err);
-    res.status(500).send('Internal Server Error');
-  }
-});
 
 
 app.get('/about', async (req, res) => {
@@ -114,7 +76,7 @@ app.get('/about', async (req, res) => {
 
 
       // Fetch data from MongoDB
-      const itemData = await collection.find({objectType: 'shopItem' }).limit(6).toArray();
+      const itemData = await collection.find({objectType: 'shopItem' }).limit(3).toArray();
 
       // Render the EJS template and pass the data
       res.render('about', { itemData});
@@ -283,37 +245,59 @@ app.get('/shop-dashboard', async (req, res) => {
   }
 });
 
-app.get('/contact', async (req, res) => {
-  if(myUserObjectType != 'admin') {
+ app.get('/contact', async (req, res) => {
+   if(myUserObjectType != 'admin') {
+     try {
+       const client = await MongoClient.connect(uri, options);
+      const db = client.db();
+       const collection = db.collection('users');
+      
+       // Fetch data from MongoDB
+       const data = await collection.find({objectType: 'shopItem'}).toArray();
+
+       // Render the EJS template and pass the data
+       res.render('contactus', { data });
+
+       // Remember to close the MongoDB connection
+       client.close();
+     } catch (err) {
+       console.error('Error fetching data from MongoDB:', err);
+      res.status(500).send('Internal Server Error');
+     } 
+   } else {
+    res.redirect('/myAccount');
+   }
+ });
+
+app.get('/myAccount', async (req, res) => {
+  //verify if myObject type is user
+  if (myUserObjectType == 'user') {
     try {
       const client = await MongoClient.connect(uri, options);
       const db = client.db();
       const collection = db.collection('users');
       
       // Fetch data from MongoDB
-      const data = await collection.find({objectType: 'shopItem'}).toArray();
+      const userInfo = await collection.find({_id: myUserID}).toArray();
+      console.log(userInfo);
+      // const formattedOutput = JSON.stringify(userInfo, null, 2);
+
+      // console.log(formattedOutput)
 
       // Render the EJS template and pass the data
-      res.render('contactus', { data });
-
+      res.render('myAccount'/*, { formattedOutput }*/);
+  
       // Remember to close the MongoDB connection
       client.close();
     } catch (err) {
       console.error('Error fetching data from MongoDB:', err);
       res.status(500).send('Internal Server Error');
     } 
-  } else {
-    res.redirect('/myAccount');
-  }
-});
-
-app.get('/myAccount', async (req, res) => {
-  //verify if myObject type is user
-  if (myUserObjectType == 'user' || myUserObjectType == 'admin') {
+  } else if(myUserObjectType == 'admin') {
     try {
       const client = await MongoClient.connect(uri, options);
       const db = client.db();
-      const collection = db.collection('users');
+      const collection = db.collection('admin', );
       
       // Fetch data from MongoDB
       const userInfo = await collection.find({_id: myUserID}).toArray();
@@ -329,6 +313,7 @@ app.get('/myAccount', async (req, res) => {
     } 
   } else {
     res.redirect('/login-user');
+
   }
 });
 
@@ -354,99 +339,90 @@ app.get('/shop', async (req, res) => {
 });
 
 app.get('/mycart', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(uri, options);
+    const db = client.db();
+    const collection = db.collection('userCarts');
 
+    
+
+    // Fetch data from MongoDB
+    const data = await collection.find({objectType: 'shopItem'}).limit(3).toArray();
+
+    // Render the EJS template and pass the data
+    res.render('shop', { data });
+
+    // Remember to close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.error('Error fetching data from MongoDB:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.post('/usr-addToCart', async (req, res) => {
-  if(myUserObjectType == 'user') {
-    const { itmID, itmQty } = req.body;
+  if(myUserObjectType != 'user') {
+    const { itmID, itmImgLnk, itmTitle, itmPrice, itmQuant, orderPrice } = req.body;
     console.log('Received request body:', req.body);
-    
-    // GET itmID and itmQty from the request body
-    const itmID2 = req.body.itmID;
-    const itmQty2 = req.body.itmQty;
-    console.log(itmID2);
-    console.log(itmQty2);
-
+    // get individual data from the request body
+      
     try {
-      if (!db) {
-        console.log('Database connection is not established yet.');
-        return res.status(500).json({ error: 'Database connection is not ready.' });
-      }
-
+      const client = await MongoClient.connect(uri, options);
+      const db = client.db();
+      const collection = db.collection('user');
+      
+      
       let pushquery ="" 
-      // combine the query that will be pushed to the array
-      pushquery = pushquery.concat("id: '", itmID, "', quantity: ", itmQty);
-      console.log(pushquery);
+      const formattedOutput = JSON.stringify(req.body, null, 2);
+      console.log(formattedOutput);
 
-      //update the carItems array variable for the user
-      const result = await db.collection(collectionName).updateOne(
-        { _id: myUserID },
-        // insert itmID and itmQty into the cartItems array variable
-        { $push: { cartItems: req.body }}
-      );
 
-      
 
-      // print the cartItems 
-      console.log(myUserID);
+        
 
-      console.log('Added to Successfully!', req.body);
-      res.status(201).json({ message: 'Added to successfully!' });
-      
-      updateStocksLeft(itmID, itmQty);
-
+      // Remember to close the MongoDB connection
+      client.close();
     } catch (err) {
-      console.error('An error occurred while inserting the record.', err);
-      return res.status(500).json({ error: 'An error occurred while inserting the record.' });
+      console.error('Error fetching data from MongoDB:', err);
+      res.status(500).send('Internal Server Error');
     }
   } else {
+    // alert('You must be logged in to add items to your cart.');
     res.redirect('/login-user');
-
   }
+  
 });
 
 
-async function updateStocksLeft(id, quantity) {
-  try {
-    if (!db) {
-      console.log('Database connection is not established yet.');
-      return { success: false, message: 'Database connection is not ready.' };
-    } else {
-      console.log 
-    }
 
-    // Make sure 'quantity' is a positive integer
-    quantity = parseInt(quantity, 10);
-    if (isNaN(quantity) || quantity <= 0) {
-      return { success: false, message: 'Please provide a valid positive quantity.' };
-    }
+            app.post('/contact-form-submit', async (req, res) => {
+              const { name, email, message } = req.body;
 
-    // Find the product by 'id'
-    const product = await db.collection(collectionName).findOne({ id });
+             
+              if (!name || !email || !message) {
+                return res.status(400).json({ error: 'Please fill out all fields in the form.' });
+              }
 
-    if (!collectionName) {
-      return { success: false, message: 'Product not found.' };
-    }
+              try {
+                if (!db) {
+                  console.log('Database connection is not established yet.');
+                  return res.status(500).json({ error: 'Database connection is not ready.' });
+                }
 
-    // Calculate the new 'stocksLeft' value
-    const updatedStocksLeft = Math.max(product.stocksLeft - quantity, 0);
+                // Save the contact form data to the database
+                await db.collection(messagesCollectionName).insertOne({
+                  name,
+                  email,
+                  message,
+                });
 
-    // Update the 'stocksLeft' in the database
-    await db.collection(collectionName).updateOne(
-      { _id },
-      { $set: { stocksLeft: updatedStocksLeft } }
-    );
-
-    return { success: true, message: 'Stocks updated successfully.' };
-  } catch (err) {
-    console.error('An error occurred while updating stocksLeft:', err);
-    return { success: false, message: 'An error occurred while updating stocksLeft.' };
-  }
-}
-
-
-
+                console.log('Message inserted into the "messages" collection successfully!');
+                res.status(201).json({ message: 'Message sent successfully!' });
+              } catch (err) {
+                console.error('Error inserting message into the "messages" collection:', err);
+                res.status(500).json({ error: 'An error occurred while sending the message.' });
+              }
+            });
 
 
 
